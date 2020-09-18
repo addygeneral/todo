d236bee758517252fc88d8664abd0cdaae32cf64
addygenerale <sammcaddy@gmail.com>
2020-09-18 05:59:41 +0000

    Updates

---
+++ precache-manifest.ec485bac5920f380978e6b63f9743b66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c36073322e42e72f6ddf0cb79baf26da",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "9e0423e83369015d1750",
+    "url": "/todo/static/css/main.4ff2c290.chunk.css"
+  },
+  {
+    "revision": "9e4a73417c4595da5db3",
+    "url": "/todo/static/js/2.38d296fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.38d296fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e0423e83369015d1750",
+    "url": "/todo/static/js/main.824371d8.chunk.js"
+  },
+  {
+    "revision": "ac37ac877329f6fc5c74",
+    "url": "/todo/static/js/runtime-main.5db1383b.js"
+  }
+]);
